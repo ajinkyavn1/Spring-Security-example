c6258e2f6418c8a1e15ee8e76ba8578c9d19f0b0
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-07-17 22:14:33 +0530

    implemented user Deatails service

--- src/main/java/com/spring/springsecurity/Security/ApplicationSecurityConfig.java
+++ src/main/java/com/spring/springsecurity/Security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +19,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return super.userDetailsService();
     }
 }
