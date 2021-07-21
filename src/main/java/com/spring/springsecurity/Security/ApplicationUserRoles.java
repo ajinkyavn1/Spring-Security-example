8ceb163cfedc685b0ad64af726649ed562add877
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-07-21 10:23:30 +0530

    Application user Roles Initialized

--- src/main/java/com/spring/springsecurity/Security/ApplicationUserRoles.java
+++ src/main/java/com/spring/springsecurity/Security/ApplicationUserRoles.java
@@ -1,6 +1,22 @@
 package com.spring.springsecurity.Security;
 
+import com.google.common.collect.Sets;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.spring.springsecurity.Security.ApplicationUserPermisions.*;
+
 public enum ApplicationUserRoles {
-    STUDENT,
-    ADMIN
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ,COURSE_WRITE,STUDENT_READ,STUDENT_WRITE));
+    private  final Set<ApplicationUserPermisions> permisions;
+
+    ApplicationUserRoles(Set<ApplicationUserPermisions> permisions) {
+        this.permisions = permisions;
+    }
+
+    public Set<ApplicationUserPermisions> getPermisions() {
+        return permisions;
+    }
 }
