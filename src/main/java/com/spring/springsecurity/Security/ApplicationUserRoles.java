707d5a0a95704b081c3e680a1e2e7dc03b35dac9
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-07-29 00:37:06 +0530

    Roles Set Created

--- src/main/java/com/spring/springsecurity/Security/ApplicationUserRoles.java
+++ src/main/java/com/spring/springsecurity/Security/ApplicationUserRoles.java
@@ -1,9 +1,12 @@
 package com.spring.springsecurity.Security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.spring.springsecurity.Security.ApplicationUserPermisions.*;
 
@@ -19,5 +22,12 @@ public enum ApplicationUserRoles {
 
     public Set<ApplicationUserPermisions> getPermisions() {
         return permisions;
+    }
+    public  Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions= getPermisions().stream()
+                .map(permisions->new SimpleGrantedAuthority(permisions.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE"+this.name()));
+        return  permissions;
     }
 }
